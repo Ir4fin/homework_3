97bd4b80a4b1c16dea1f81d0307307e73fa3ad15
Ir4fin <messu91@gmail.com>
2022-08-04 19:02:19 +0500

    changed First test

--- src/test/java/guru/qa/HomeWork.java
+++ src/test/java/guru/qa/HomeWork.java
@@ -6,6 +6,10 @@ import org.junit.jupiter.api.Test;
 public class HomeWork {
     @Test
     void firstTest(){
-        Assertions.assertTrue((2048 - 15) > (100));
+        Assertions.assertTrue((2048 - 15) > (95));
+    }
+    @Test
+    void secondTest (){
+        Assertions.assertFalse((850-50) < 200);
     }
 }
